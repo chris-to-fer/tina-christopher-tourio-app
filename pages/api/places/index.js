03849caa280af8f34e4566a7093806b084850b2d
Tina Huynh Le <huynhletina@gmail.com>
2024-02-13 17:09:34 +0100

    write delete function

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -3,7 +3,6 @@ import { Place } from "../../../db/models/Place";
 
 export default async function handler(request, response) {
   await connectDB();
-  const { id } = request.query;
 
   if (request.method === "GET") {
     const places = await Place.find();
@@ -19,11 +18,6 @@ export default async function handler(request, response) {
     } catch (error) {
       response.status(400).json({ error: error.message });
     }
-  }
-
-  if (request.method === "DELETE") {
-    await Place.findByIdAndDelete(id);
-    response.status(200).json({ status: `Place ${id} successfully deleted.` });
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
